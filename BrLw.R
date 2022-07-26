c56a69e671b312ca0142d8d565709008247e0a0f
Precious Calderon <pcalderon3@avc.edu>
2022-07-26 21:53:15 +0000

    edited answer to question 2

--- BrLw.R
+++ BrLw.R
@@ -1,14 +1,12 @@
 BrLw <-function(numClass=35,odds=2,countoDo=637){
-
-days=1:365
+  
+  days=1:365
   brownlow = 0
-  for (i in 1:countoDo)BrLw() {
+  for (i in 1:countoDo) {
     sam=sample(days,size=numClass,replace = T)
     y=duplicated(sam)
     if (sum(y)>0) brownlow=brownlow+1
     else brownlow = brownlow - odds
   }
- return(brownlow)
+  return(brownlow)
 }
-
-BrLw40 <- BrLW(odds = 40)
